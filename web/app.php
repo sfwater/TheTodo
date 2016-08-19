2bde14b1058b6cda995542535a184cf40b3f7b91
Tilen <tilen.gombac@gmail.com>
2016-08-20 01:33:42 +0200

    UI bugfixes

--- web/app.php
+++ web/app.php
@@ -14,7 +14,9 @@ $kernel->loadClassCache();
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
+
 $kernel->terminate($request, $response);
